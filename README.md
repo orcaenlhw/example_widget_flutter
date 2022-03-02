39fb8451adfb8389bffd43fd8740649de0bf3b46
edn <45616806+theednorc@users.noreply.github.com>
2022-03-02 21:44:12 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# example_widget_flutter
+ 
---
+++ android/app/src/main/kotlin/com/example/example_widget_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_widget_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
